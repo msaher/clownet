98e665bd142dfa87d3a8f2c8c6e6c4890ba2fd53
saher <msaher.shair@gmail.com>
2024-04-11 19:52:03 +0300

    add setup.sh script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+apt install python3-dev libavutil-dev libavformat-dev libswscale-dev
+pip install timm
+
+
+# get the coviar data loader
+cd data pytorch-coviar/data_loader/ && ./install.sh

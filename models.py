eaf3ff3e17cfe9762ab5e4cccdb857b9093597b7
saher <msaher.shair@gmail.com>
2024-04-13 15:23:31 +0300

    fix: make mobile former in gpu

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ from pycoviar.transforms import GroupMultiScaleCrop
 # loading the mobile former pretrained net for embedding images
 mobile = mobile_former_26m(pretrained=False)
 
-state = torch.load("./mobileformer/mobile-former-26m.pth.tar", map_location=torch.device('cpu'))['state_dict']
+state = torch.load("clownet/mobileformer/mobile-former-26m.pth.tar")['state_dict']
 
 mobile.load_state_dict(state)
 
